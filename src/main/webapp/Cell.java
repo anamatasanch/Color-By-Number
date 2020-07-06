e2c0bae00ba19aa607f3a059f6763239cfc2748b
B'Elanna Barnes <belannabarnes@wustl.edu>
2020-07-06 12:46:02 -0500

    added a Cell object that will contain an id (that we can call) and the color that is Needed for that cell. ideally if the pen color matches the colorNeeded for the cell, then it would change, and if not, the cell would remain blank.

---
+++ src/main/webapp/Cell.java
@@ -0,0 +1,12 @@
+
+/** An cell in an image. */
+public final class Cell {
+
+  private final String id;
+  private final String colorNeeded;
+
+  public Cell(String id, String colorNeeded) {
+    this.id = id;
+    this.colorNeeded = colorNeeded;
+  }
+}
